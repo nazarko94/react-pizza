d3e2a321aaf24c5e6536560092daee493477d08b
nazarko94 <nazarko94@gmail.com>
2021-12-30 18:04:19 +0200

    lesson-7

--- src/componets/Categories.jsx
+++ src/componets/Categories.jsx
@@ -1,41 +1,11 @@
 import React from 'react';
 
-// class Categories extends React.Component {
-//     state= {
-//         activeItem: 3,
-//     }
-
-//     onSelectItem = index => {
-//         this.setState({
-//             activeItem: index,
-//         });
-//     }
-
-//     render() {
-//         const {items, onClickItem} = this.props;
-//         return (
-//             <div className="categories">
-//                <ul>
-//                  <li>Всі</li>
-//                 {items.map((name, index) => (
-//                     <li 
-//                     className={this.state.activeItem === index ? 'active' : ''}
-//                     onClick={() => this.onSelectItem(index)}
-//                     key={`${name}_${index}`}>
-//                     {name}
-//                 </li>
-//                 ))}
-//               </ul>
-//         </div>
-//         )
-//     }
-// }
-
-const Categories = ({items, onClickItem}) => {
+const Categories = React.memo(function Categories ({items, onClickItem}) {
     const [activeItem, setActiveItem] = React.useState(null);
 
     const onSelectItem = (index) => {
         setActiveItem(index);
+        onClickItem(index);
     }
 
     return (
@@ -54,6 +24,6 @@ const Categories = ({items, onClickItem}) => {
               </ul>
         </div>
     )
-}
+});
 
 export default Categories;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,39 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
 import { Categories, SortPopup, PizzaBlock } from "../componets";
 
-const Home = ({ items }) => {
+import { setCategory } from "../redux/actions/filters";
+
+const categoryNames = ["Мясні", "Вегетаріанські", "Гриль", "Гострі", "Закриті"];
+const sortItems = [
+  { name: "популярності", type: "popular" },
+  { name: "ціні", type: "price" },
+  { name: "алфавіту", type: "alphabet" },
+];
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const items = useSelector(({ pizzas }) => pizzas.items);
+
+  const onSelectCategory = React.useCallback((index) => {
+    dispatch(setCategory(index));
+    }, []);
+
   return (
     <div className="container">
       <div className="content__top">
         <Categories
-          onClickItem={(name) => console.log(name)}
-          items={["Мясні", "Вегетаріанські", "Гриль", "Гострі", "Закриті"]}
+          onClickItem={onSelectCategory}
+          items={categoryNames}
         />
-        <SortPopup items={[
-        {name: "популярності", type: "popular"}, 
-        {name: "ціні", type: "price"}, 
-        {name: "алфавіту", type: "alphabet"}
-        ]} />
+        <SortPopup
+          items={sortItems}
+        />
       </div>
       <h2 className="content__title">Всі піци</h2>
       <div className="content__items">
-        {items.map((obj) => (
-          <PizzaBlock key={obj.id} {...obj}/>
-        ))}
+        {items && items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
     </div>
   );
